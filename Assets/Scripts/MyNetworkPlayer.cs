d7af70cafc0a4ffc19f41434322d51b03a206cc2
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-06-07 16:40:11 -0300

    adds a color to be a SyncVar and a method to randomize the color

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -9,9 +9,22 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SerializeField]
     private string displayName = "Missing Name";
 
+    [SyncVar]
+    [SerializeField]
+    private Color sphereColor = new Color();
+
     [Server]
     public void SetDisplayNameTo(string aNewDisplayName)
     {
         displayName = aNewDisplayName;
+    }
+
+    [Server]
+    public void RandomizeSphereColor()
+    {
+        sphereColor = new Color(Random.Range(0, 255), 
+                                            Random.Range(0, 255), 
+                                            Random.Range(0, 255), 
+                                            (float)Random.Range(0, 100)/100);
     }
 }

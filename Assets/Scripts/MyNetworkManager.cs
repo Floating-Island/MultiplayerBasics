a54f9c7c04e0f0680c58f6c630042a8c9981a8ce
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-06-07 15:28:50 -0300

    Adds debug message to OnClientConect override

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -3,8 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyNetworkManager : NetworkManager
+public class MyNetworkManager : NetworkManager 
 {
-    
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I Connected to a server!");
+    }
 }
-  

9e59410539605e514c09b50ada810dc0f857fd74
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-06-08 16:56:19 -0300

    Creates handle to update display name text when replicated

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -31,4 +31,9 @@ public class MyNetworkPlayer : NetworkBehaviour
                                 Random.Range(0f, 1f), 
                                 Random.Range(0f, 1f));
     }
+
+    private void HandleDisplayNameUpdate(string anOldDisplayName, string aNewDisplayName)
+    {
+        displayNameText.text = aNewDisplayName;
+    }
 }

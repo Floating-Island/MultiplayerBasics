5459250390227e344cd4d7d509da2255f5a0e4ec
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-06-10 19:20:01 -0300

    Creates command to move a Player to a destination.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,17 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    [Command]
-    public void CmdMove()
-    {
+    [SerializeField]
+    private NavMeshAgent agent = null;
 
+    [Command]
+    public void CmdMoveTo(Vector3 aPosition)
+    {
+        agent.SetDestination(aPosition);
     }
 }
